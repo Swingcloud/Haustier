f48530579264201a6491b8c0796545c035dbb7b4
Sam Lin <greenlin111@gmail.com>
2016-12-23 00:03:16 +0800

    fix action_cable render issue

--- app/jobs/notifications_broadcast_job.rb
+++ app/jobs/notifications_broadcast_job.rb
@@ -2,28 +2,30 @@ class NotificationsBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(pet)
-    pet_info = render_pet_info(pet)
-    customers = Customer.all
-    customers.each do |customer|
-    	if customer.preference == {}
-    		pet_brodcast(customer, pet_info)
-    	elsif pet.breed.nil? && pet.type == customer.species && pet.age == customer.age
-    		pet_brodcast(customer, pet_info)
- 			elsif pet.breed == customer.breed && pet.type == customer.species && pet.age == customer.age
- 				pet_brodcast(customer, pet_info)
- 			end  
- 		end 
+    @customers = Customer.all
+    @customers.each do |customer|
+      if customer.preference == {}
+        pet_info = render_pet_info(pet, customer)
+        pet_brodcast(customer, pet_info)
+      elsif pet.breed.nil? && pet.type == customer.species && pet.age == customer.age
+        pet_info = render_pet_info(pet, customer)
+        pet_brodcast(customer, pet_info)
+      elsif pet.breed == customer.breed && pet.type == customer.species && pet.age == customer.age
+        pet_info = render_pet_info(pet, customer)
+        pet_brodcast(customer, pet_info)
+      end  
+    end 
   end
 
   private
 
-  def render_pet_info(pet)
+  def render_pet_info(pet, customer)
       CustomersController.render partial: 'customers/pet',
-                            		 locals: {pet: pet}
+                                 locals: {pet: pet, customer: customer}                           
   end
 
   def pet_brodcast(customer, pet_info)
-  	ActionCable.server.broadcast "notification_#{customer.id}_channel",
+    ActionCable.server.broadcast "notification_#{customer.id}_channel",
                                  pet_info: pet_info
   end
 
